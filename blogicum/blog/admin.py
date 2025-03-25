1032fd4c60b07acd806e4f33d1113d1f3db083d4
ShperLuck <nikitaboickov4@gmail.com>
2025-03-25 22:04:27 +0300

    Delete blogicum/blog directory

--- blogicum/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin  # Модуль для админки Django
-
-from .models import Category, Location, Post  # Импортируем наши модели из models.py
-
-admin.site.register(Category)  # Регистрируем модель Category в админке
-admin.site.register(Location)  # Регистрируем модель Location
-admin.site.register(Post)  # Регистрируем модель Post
